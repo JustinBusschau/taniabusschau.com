48c32f37169ac83bb27035674624a19de37fd5cf
Justin Busschau <JustinBusschau@users.noreply.github.com>
2022-05-28 23:28:51 +0100

    not always localhost :bug:

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import fetch from 'cross-fetch'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000',
+    uri: process.env.TANIA_BUSSCHAU_GRAPHQL,
     fetch,
   }),
   cache: new InMemoryCache(),
